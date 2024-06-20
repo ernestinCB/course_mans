8edcbf82e5eff20ed8bb44bf64498fc2e9a9716d
ernestinCB <ernestincabaret@gmail.com>
2024-06-20 14:06:30 +0200

    ajout des fonctionnalites javascript

---
+++ jeu.js
@@ -0,0 +1,27 @@
+
+let tirageAleatoire = Math.random();
+console.log(tirageAleatoire);
+
+tirageAleatoire = tirageAleatoire * 100;
+console.log(tirageAleatoire);
+
+tirageAleatoire = Math.round(tirageAleatoire);
+console.log(tirageAleatoire);
+
+let jeuContinue = true;
+while (jeuContinue) {
+
+    let valeurChoisiParUtilisateur = prompt('Trouve le nombre');
+    console.log(valeurChoisiParUtilisateur);
+
+    if (valeurChoisiParUtilisateur == tirageAleatoire) {
+        alert('Bravo !');
+        jeuContinue = false;
+    } else if (valeurChoisiParUtilisateur > tirageAleatoire) {
+        alert("C'est moins !");
+    } else {
+        alert("C'est plus !");
+    }
+
+}
+
