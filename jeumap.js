bba3e98b444de76436f555f428bb9c0f6b34b94a
ernestinCB <ernestincabaret@gmail.com>
2024-06-21 16:06:42 +0200

    sauvegarde

--- jeumap.js
+++ jeumap.js
@@ -49,25 +49,57 @@ L.geoJSON(GEOPHOTO_DATA, {
 
 // console.log(photos);
 
-let tirage = Math.random() * (photos.length - 1)
-tirage = Math.round(tirage);
-
-let selectedPhoto = photos[tirage];
-let selectedCoordinates = coordinates[tirage];
-console.log(selectedPhoto, selectedCoordinates);
-
-const image = document.createElement('img');
-image.src = selectedPhoto;
-image.classList.add('photo-choisie');
-
-let listephoto = document.getElementById("photo");
-listephoto.appendChild(image);
-
 
 map.on('click', (event) => {
-    console.log('click', event, event.latlng); 
+    console.log('click', event, event.latlng);
     var marker = L.marker(selectedCoordinates).addTo(map);
 
+    const coordinateClick = [event.latlng.lat, event.latlng.lng];
+    var markerClick = L.circle(coordinateClick, { color: 'red', radius: 10 }).addTo(map);
+
+    const latlngs = [selectedCoordinates, coordinateClick];
+
+    let polyline = L.polyline(latlngs, { color: 'red' }).addTo(map);
+    let distancephoto = map.distance(selectedCoordinates, coordinateClick);
+    
+    distancephoto = (Math.round(distancephoto))/1000;
+    console.log (distancephoto)
+    const listephoto = document.getElementById('photo');
+    const score = document.createElement('p');
+    score.innerText = new Intl.NumberFormat('fr-FR').format(distancephoto) + 'km';
+    console.log('buildScore', distancephoto, selectedCoordinates, coordinateClick);
+    listephoto.insertBefore(score, listephoto.firstChild);
+    map.flyTo(selectedCoordinates, 16);
+    
+    
+    
+    nouveauTirage();
 });
+
+
+let tirage;
+let selectedPhoto;
+let selectedCoordinates;
+let listephoto = document.getElementById("photo");
+
+const nouveauTirage = () => {
+    tirage = Math.random() * (photos.length - 1)
+    tirage = Math.round(tirage);
+
+    selectedPhoto = photos[tirage];
+    selectedCoordinates = coordinates[tirage];
+    console.log(selectedPhoto, selectedCoordinates);
+
+    const image = document.createElement('img');
+    image.src = selectedPhoto;
+    image.classList.add('photo-choisie');
+    
+
+    listephoto.insertBefore(image, listephoto.firstChild);
+}
+nouveauTirage();
+
+
+
 
 
